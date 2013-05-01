c1ef9a2bb3a3e2396a8782ce465200fc03d3d6a5
Trystan <trystan@trystan-laptop.(none)>
2013-05-01 08:51:22 +0100

    minor

--- raspberrypi_run.php
+++ raspberrypi_run.php
@@ -67,6 +67,11 @@
   $frequency = $settings->frequency;
   $baseid = $settings->baseid;
 
+  $remoteprotocol = $settings->remoteprotocol;
+  $remotedomain = $settings->remotedomain;
+  $remotepath = $settings->remotepath;
+  $remoteapikey = $settings->remoteapikey;
+
   // Create a stream context that configures the serial port
   // And enables canonical input.
   $c = stream_context_create(array('dio' =>
@@ -145,7 +150,7 @@
         $remotedata .= "]";
         echo "Sending remote data";
         //echo $remotedata."\n";
-        getcontent($settings->remotedomain,80,$settings->remotepath."/input/bulk.json?apikey=".$settings->remoteapikey."&data=".$settings->remotedata);
+        getcontent($settings->remotedomain,80,$settings->remotepath."/input/bulk.json?apikey=".$settings->remoteapikey."&data=".$remotedata);
         $ni = 0; $remotedata = "[";
         $start_time = time();
       }
